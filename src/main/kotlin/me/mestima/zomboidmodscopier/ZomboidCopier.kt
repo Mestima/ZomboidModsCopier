a231eb993a38d9212d5ec11b2da460108162a11a
Mestima <mestima@nilored.ru>
2022-10-11 23:16:47 +0300

    bump new version

--- src/main/kotlin/me/mestima/zomboidmodscopier/ZomboidCopier.kt
+++ src/main/kotlin/me/mestima/zomboidmodscopier/ZomboidCopier.kt
@@ -9,7 +9,7 @@ class ZomboidCopier : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(ZomboidCopier::class.java.getResource("main.fxml"))
         val scene = Scene(fxmlLoader.load(), 483.0, 281.0)
-        stage.title = "Zomboid mods copier v1.0"
+        stage.title = "Zomboid mods copier v2.0"
         stage.scene = scene
         stage.show()
     }
